c58832e99a0b3276f6cbdee290d94b55dd4eb89a
AleksBeliun <aleksandr@novoresume.com>
2022-07-10 16:04:44 +0200

    build: :wrench: Add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
+
+module.exports = {
+  mode: "jit",
+  darkMode: "class",
+  content: ["./**/*.{ts,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+      serif: ["Georgia", "serif"]
+    }
+  },
+  variants: {
+    extend: {
+      typography: ["dark"]
+    }
+  },
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")]
+}
