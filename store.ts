b7c8528ed583266e9611a462ab44cdd5d918cd69
AleksBeliun <aleksandr@novoresume.com>
2022-07-12 13:47:16 +0200

    feat: :sparkles: Setup storage

---
+++ store.ts
@@ -0,0 +1,55 @@
+import { Store, configureStore } from "@reduxjs/toolkit"
+import { localStorage } from "redux-persist-webextension-storage"
+
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  RESYNC,
+  persistReducer,
+  persistStore
+} from "@plasmohq/redux-persist"
+import { Storage } from "@plasmohq/storage"
+
+import listSliceReducer from "~list-slice"
+
+const rootReducer = listSliceReducer
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage: localStorage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store: Store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [
+          FLUSH,
+          REHYDRATE,
+          PAUSE,
+          PERSIST,
+          PURGE,
+          REGISTER,
+          RESYNC
+        ]
+      }
+    })
+})
+export const persistor = persistStore(store)
+
+new Storage().watch({
+  [`persist:${persistConfig.key}`]: () => {
+    persistor.resync()
+  }
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
