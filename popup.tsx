63d37440d03cf94b6ff32e06e70a24a85be041eb
AleksBeliun <aleksandr@novoresume.com>
2022-07-13 20:41:26 +0200

    feat: :sparkles: Setup Store provider

--- popup.tsx
+++ popup.tsx
@@ -1,24 +1,21 @@
 import { useState } from "react"
+import { PersistGate } from '@plasmohq/redux-persist/integration/react'
+import { Provider } from 'react-redux'
+
+import { store, persistor } from '~store'
 
 import "./style.css"
+import ListView from "~listView"
 
 function IndexPopup() {
   const [data, setData] = useState("")
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h1>
-        Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
-      </h1>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      Value:
-      <span className="inline-flex items-center justify-center h-4 ml-2 text-xs font-semibold text-blue-800 bg-blue-200">{data}</span>
-    </div>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ListView />
+      </PersistGate>
+    </Provider>
   )
 }
 
