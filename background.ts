0a84bce2a372dd25f53a60c21af65c0add03082e
AleksBeliun <aleksandr@novoresume.com>
2022-07-13 20:40:26 +0200

    feat: :sparkles: See state changes from background script

--- background.ts
+++ background.ts
@@ -1,3 +1,9 @@
+import { persistor, store } from "~store"
+
+persistor.subscribe(() => {
+  console.log("State changed with: ", store?.getState())
+})
+
 export {}
 
 console.log("Hello from background script!")
