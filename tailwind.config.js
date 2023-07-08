ce96544f6dbc76db3391273a7d1d8a3a67970d83
AleksBeliun <aleksandr@nvoresume.com>
2023-07-08 12:39:16 +0200

    chore: :heavy_plus_sign: Add React Icons and DaisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,5 +15,9 @@ module.exports = {
       typography: ["dark"]
     }
   },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")]
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("daisyui")
+  ]
 }
