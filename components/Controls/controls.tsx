77a86dbad04c205e12e133c4e3581580cb00fa2f
AleksBeliun <aleksandr@nvoresume.com>
2023-07-08 13:48:56 +0200

    feat: :zap: Update controls

--- components/Controls/controls.tsx
+++ components/Controls/controls.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { VscAdd, VscDiffAdded, VscRefresh, VscTrash, VscSettingsGear } from "react-icons/vsc";
-
+import { openExtensionOptions } from "~utils"
 function Controls() {
     return (
         <div className="space-y-5">
@@ -10,7 +10,7 @@ function Controls() {
                     <button className='btn join-item text-xl flex-grow justify-center' title='Add all open tabs to the list'><VscDiffAdded /></button>
                     <button className='btn join-item text-xl flex-grow justify-center' title='Check tabs list'><VscRefresh /></button>
                     <button className='btn join-item text-xl flex-grow justify-center' title='Clear all tabs'><VscTrash /></button>
-                    <button className='btn join-item text-xl flex-grow justify-center' title='Settings'><VscSettingsGear /></button>
+                    <button className='btn join-item text-xl flex-grow justify-center' title='Settings' onClick={openExtensionOptions}><VscSettingsGear /></button>
                 </div>
             </div>
         </div>
--- popup.tsx
+++ popup.tsx
@@ -6,13 +6,16 @@ import { store, persistor } from '~store'
 
 import "./style.css"
 import ListView from "~components/List/listView"
+import Controls from "~components/Controls/controls"
 
 function IndexPopup() {
   const [data, setData] = useState("")
+  const [activeTab, setActiveTab] = useState("1")
 
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+        <Controls />
         <ListView />
       </PersistGate>
     </Provider>
