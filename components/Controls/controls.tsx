074dddfd6b0fd8e342aeb4808dd298293feb61f9
AleksBeliun <aleksandr@nvoresume.com>
2023-07-08 12:39:38 +0200

    feat: :sparkles: Add basic controls

---
+++ components/Controls/controls.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { VscAdd, VscDiffAdded, VscRefresh, VscTrash, VscSettingsGear } from "react-icons/vsc";
+
+function Controls() {
+    return (
+        <div className="space-y-5">
+            <div className=" border-b border-b-gray-200">
+                <div className="join w-full">
+                    <button className='btn join-item text-xl flex-grow justify-center' title='Add current tab to the list'><VscAdd /></button>
+                    <button className='btn join-item text-xl flex-grow justify-center' title='Add all open tabs to the list'><VscDiffAdded /></button>
+                    <button className='btn join-item text-xl flex-grow justify-center' title='Check tabs list'><VscRefresh /></button>
+                    <button className='btn join-item text-xl flex-grow justify-center' title='Clear all tabs'><VscTrash /></button>
+                    <button className='btn join-item text-xl flex-grow justify-center' title='Settings'><VscSettingsGear /></button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Controls
