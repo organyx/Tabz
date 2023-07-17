7ca2af4a033b3d6cbee207e17329fc4c7df7a79d
AleksBeliun <aleksandr@nvoresume.com>
2023-07-17 21:30:14 +0200

    feat: :sparkles: Display icon instead of a span

--- components/Item/item.tsx
+++ components/Item/item.tsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import { remove, type ListItem } from "~components/List/list-slice";
-import { useAppDispatch } from '~store'; import { VscAdd, VscDiffAdded, VscRefresh, VscTrash, } from "react-icons/vsc";
+import { VscTrash } from "react-icons/vsc"
 
+import { type ListItem, remove } from "~components/List/list-slice"
+import { useAppDispatch } from "~store"
 
-function Item({ item, index }: { item: ListItem, index: number }) {
-    const dispatch = useAppDispatch();
-    const removeItemHandler = (index: number): void => {
-        dispatch(remove({ index }));
-    }
+function Item({ item, index }: { item: ListItem; index: number }) {
+  const dispatch = useAppDispatch()
+  const removeItemHandler = (index: number): void => {
+    dispatch(remove({ index }))
+  }
 
-    return (
-        <li className='flex flex-1 justify-center items-center text-lg'><span>{item.icon}</span><a href={item.url}>{item.title}</a>  <VscTrash onClick={() => removeItemHandler(index)} /></li>
-
-
-    )
+  return (
+    <li className="flex flex-1 justify-center items-center text-sm">
+      <img src={item.icon} width="15px" />
+      <a href={item.url}>{item.title}</a>
+      <VscTrash onClick={() => removeItemHandler(index)} />
+    </li>
+  )
 }
 
 export default Item
