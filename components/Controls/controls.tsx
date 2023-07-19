2f94dac9107b96b0429874f75e2023503f29e68c
AleksBeliun <aleksandr@nvoresume.com>
2023-07-19 21:31:58 +0200

    feat: :zap: Move handlers from view to controls

--- components/Controls/controls.tsx
+++ components/Controls/controls.tsx
@@ -6,16 +6,33 @@ import {
   VscTrash
 } from "react-icons/vsc"
 
+import { add, clear } from "~components/List/list-slice"
+import { useAppDispatch } from "~store"
 import { openExtensionOptions } from "~utils"
 
 function Controls() {
+  const dispatch = useAppDispatch()
+
+  const addItemHandler = async (event): Promise<void> => {
+    const tabs = await chrome.tabs.query({ active: true, currentWindow: true })
+    const { id, title, url, favIconUrl } = tabs[0]
+    const item = { icon: favIconUrl, title, url, id }
+    console.log("🚀 ~ file: listView.tsx:28 ~ addItemHandler2 ~ item:", item)
+    dispatch(add(item))
+  }
+
+  const clearAllHandler = (): void => {
+    dispatch(clear())
+  }
+
   return (
     <div className="space-y-5">
       <div className=" border-b border-b-gray-200">
         <div className="join w-full">
           <button
             className="btn join-item text-xl flex-grow justify-center"
-            title="Add current tab to the list">
+            title="Add current tab to the list"
+            onClick={addItemHandler}>
             <VscAdd />
           </button>
           <button
@@ -30,7 +47,8 @@ function Controls() {
           </button>
           <button
             className="btn join-item text-xl flex-grow justify-center"
-            title="Clear all tabs">
+            title="Clear all tabs"
+            onClick={clearAllHandler}>
             <VscTrash />
           </button>
           <button
--- components/List/listView.tsx
+++ components/List/listView.tsx
@@ -1,43 +1,21 @@
-import { useRef } from "react"
-
 import Item from "~components/Item/item"
 import type { ListState } from "~components/List/list-slice"
-import { add, clear } from "~components/List/list-slice"
-import { useAppDispatch, useAppSelector } from "~store"
+import { useAppSelector } from "~store"
 
 function ListView() {
-  const dispatch = useAppDispatch()
   const listValue = useAppSelector((state: ListState) => state.list)
-  const inputRef = useRef<HTMLInputElement>(null)
-
-  const addItemHandler = async (event): Promise<void> => {
-    const tabs = await chrome.tabs.query({ active: true, currentWindow: true })
-    const { id, title, url, favIconUrl } = tabs[0]
-    const item = { icon: favIconUrl, title, url, id }
-    console.log("🚀 ~ file: listView.tsx:28 ~ addItemHandler2 ~ item:", item)
-    dispatch(add(item))
-  }
-
-  const clearAllHandler = (): void => {
-    dispatch(clear())
-  }
 
   return (
     <div className="flex flex-col p-1 h-52 max-h-56">
-      {listValue.length > 0 && (
+      {listValue.length > 0 ? (
         <ul className="flex flex-col">
           {listValue.map((item, index) => (
             <Item key={index} item={item} index={index} />
           ))}
         </ul>
+      ) : (
+        <p className="text-center text-gray-400">No tabs</p>
       )}
-
-      <button className="h-10 w-50 text-blue-400" onClick={addItemHandler}>
-        Add To Storage
-      </button>
-      <button className="h-10 w-50 text-blue-400" onClick={clearAllHandler}>
-        Clear All
-      </button>
     </div>
   )
 }
