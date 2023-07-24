42c3953ecbc3251bd76d31a7ca15162367a178b3
AleksBeliun <aleksandr@nvoresume.com>
2023-07-24 22:05:06 +0200

    feat: :sparkles: Add handler for single tab to bookmark

--- components/Item/item.tsx
+++ components/Item/item.tsx
@@ -1,12 +1,33 @@
-import { VscTrash } from "react-icons/vsc"
+import { VscBookmark, VscTrash } from "react-icons/vsc"
 
 import { type ListItem, remove } from "~components/List/list-slice"
-import { useAppDispatch } from "~store"
+import { useAppDispatch, useAppSelector } from "~store"
 
 function Item({ item, index }: { item: ListItem; index: number }) {
   const dispatch = useAppDispatch()
+  const bookmarkFolder = useAppSelector((state) => state.bookmarkFolder)
   const removeItemHandler = (index: number): void => {
     dispatch(remove({ index }))
+  }
+
+  const tabToBookMarkHandler = async (): Promise<void> => {
+    const confirm = window.confirm(
+      "Are you sure you want to convert this tab to a bookmark?"
+    )
+    if (confirm) {
+      console.log("tabToBookMarkHandler", bookmarkFolder)
+      const bookmark = {
+        parentId: bookmarkFolder.id,
+        title: item.title,
+        url: item.url
+      }
+
+      const created = await chrome.bookmarks.create(bookmark)
+      console.log(
+        "🚀 ~ file: item.tsx:26 ~ tabToBookMarkHandler ~ created:",
+        created
+      )
+    }
   }
 
   return (
@@ -15,6 +36,10 @@ function Item({ item, index }: { item: ListItem; index: number }) {
       <a href={item.url} target="_blank" className="w-64">
         {item.title}
       </a>
+      <VscBookmark
+        onClick={() => tabToBookMarkHandler()}
+        className="group w-6 hover:text-blue-500 active:scale-90 transition-all"
+      />
       <VscTrash
         onClick={() => removeItemHandler(index)}
         className="group w-6 hover:text-red-500 active:scale-90 transition-all"
