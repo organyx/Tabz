4f35273df9fa55ac80c81938c9602dd520d7c33c
AleksBeliun <aleksandr@nvoresume.com>
2023-07-26 22:06:32 +0200

    feat: :sparkles: Item text is limited. Full text is in the tooltip

--- components/Item/item.tsx
+++ components/Item/item.tsx
@@ -31,11 +31,16 @@ function Item({ item, index }: { item: ListItem; index: number }) {
   }
 
   return (
-    <li className="btn flex flex-1 flex-nowrap justify-around items-center text-sm">
+    <li className="btn flex flex-1 flex-nowrap justify-around items-center px-0">
       <img src={item.icon} className="w-6 p-1" />
-      <a href={item.url} target="_blank" className="w-64">
-        {item.title}
-      </a>
+      <div className="tooltip" data-tip={item.title}>
+        <a
+          href={item.url}
+          target="_blank"
+          className="w-64 inline-block overflow-hidden text-ellipsis whitespace-nowrap text-xs">
+          {item.title}
+        </a>
+      </div>
       <VscBookmark
         onClick={() => tabToBookMarkHandler()}
         className="group w-6 hover:text-blue-500 active:scale-90 transition-all"
